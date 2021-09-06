94df29cd37cfea693a86c8c3b6a73527bc838a69
Anqi Zhang <blesssong2020@gmail.com>
2021-09-06 15:38:12 -0400

    tuple learning

---
+++ my_tuple_learning.py
@@ -0,0 +1,14 @@
+#### Tuple: unchangeable, ()
+
+def my_tuple_test():
+    my_tuple = (1, 2, "hello")
+    print(my_tuple[0])
+    print(len(my_tuple))
+
+
+
+
+
+
+if __name__ == '__main__':
+    my_tuple_test()
