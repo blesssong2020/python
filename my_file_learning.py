2cb95af337e087e2de95adbdd87c0fb3ff67e419
Anqi Zhang <blesssong2020@gmail.com>
2021-09-06 22:58:27 -0400

    my dictionary homework

--- my_file_learning.py
+++ my_file_learning.py
@@ -47,6 +47,19 @@ def my_file_processing():
                 word_freq[w] += 1
             else:
                 word_freq[w] = 1
+        print("original length of word_freq:", len(word_freq))
+
+        result_dict = {}
+        while len(word_freq) > 0:
+            min_val = min(word_freq.values())
+            new_key = list(word_freq.keys())[list(word_freq.values()).index(min_val)]
+            #result_dict[new_key] = min_val
+            new_dict = {new_key:min_val}
+            result_dict.update(new_dict)
+            word_freq.pop(new_key)
+        print("length of new sorted dictionary:", len(result_dict))
+        print(result_dict)
+
     with open("result.txt", "w") as f_2:
         for item in word_freq.items():
             f_2.write(str(item))
