755c48d6461ad4002524f3b1f522b48e5e914538
Anqi Zhang <blesssong2020@gmail.com>
2021-12-15 20:24:35 -0500

    level print

--- my_tree_learning.py
+++ my_tree_learning.py
@@ -24,7 +24,7 @@ class Binary_Tree:
 
         return result
 
-    def pre_order_stack_print(self):
+    def in_order_stack_print(self):
         result = []
         if not self.root:
             return result
@@ -44,7 +44,16 @@ class Binary_Tree:
                     break
         return result
 
-    def in_order_stack_print(self):
+    def in_order_print(self, start_node, result = ""):
+        if not start_node:
+            return result
+        result = self.in_order_print(start_node.left, result)
+        result += str(start_node.data)+"->"
+        result = self.in_order_print(start_node.right, result)
+
+        return result
+
+    def pre_order_stack_print(self):
         result = []
         if not self.root:
             return result
@@ -60,16 +69,6 @@ class Binary_Tree:
                 my_stack.push(curr_node.left)
         return result
 
-
-    def in_order_print(self, start_node, result = ""):
-        if not start_node:
-            return result
-        result = self.in_order_print(start_node.left, result)
-        result += str(start_node.data)+"->"
-        result = self.in_order_print(start_node.right, result)
-
-        return result
-
     def post_order_print(self, start_node, result = ""):
         if not start_node:
             return result
@@ -79,6 +78,137 @@ class Binary_Tree:
         result += str(start_node.data) + "->"
 
         return result
+
+    def post_order_stack_print(self, result = []):
+        my_stack = Stack()
+        my_stack.push(self.root)
+        if not self.root:
+            return result
+        while not my_stack.isEmpty():
+            curr_node = my_stack.top()
+            my_stack.pop()
+            result.append(curr_node.data)
+            if curr_node.left:
+                my_stack.push(curr_node.left)
+            if curr_node.right:
+                my_stack.push(curr_node.right)
+        result.reverse()
+        return result
+
+
+    def post_order_stack_print_3(self, result = []):
+        my_stack = Stack()
+        if not self.root:
+            return result
+        curr_node = self.root
+        while True:
+            while curr_node:
+                if curr_node.right:
+                    my_stack.push(curr_node.right)
+                my_stack.push(curr_node)
+                curr_node = curr_node.left
+            curr_node = my_stack.top()
+            my_stack.pop()
+
+            if curr_node.right == my_stack.top():
+                my_stack.pop()
+                my_stack.push(curr_node)
+                curr_node = curr_node.right
+            else:
+                result.append(curr_node.data)
+                curr_node = None
+
+            if my_stack.isEmpty():
+                break
+        return result
+
+
+    def level_print(self, result=[]):
+        my_queue = []
+        if not self.root:
+            return result
+        curr_node = self.root
+        my_queue.append(curr_node)
+        while my_queue:
+            node = my_queue.pop()
+            result.append(node.data)
+            if node.left:
+                my_queue.insert(0,node.left)
+            if node.right:
+                my_queue.insert(0,node.right)
+        return result
+
+
+
+    def post_order_stack_print_2(self):
+        result = []
+        if not self.root:
+            return result
+        my_stack = Stack()
+        curr_node = self.root
+        repeat_node = None
+        while True:
+            while curr_node:
+                my_stack.push(curr_node)
+                curr_node = curr_node.left
+
+            pre_node = my_stack.top()
+            if pre_node.right and repeat_node != pre_node.right:
+                curr_node = pre_node.right
+            else:
+                my_stack.pop()
+                result.append(pre_node.data)
+                repeat_node = pre_node
+
+            if my_stack.isEmpty():
+                break
+
+        return result
+
+    def level_reverse_print(self):
+        if not self.root:
+            return []
+        result = []
+        my_queue = [self.root]
+        while my_queue:
+            curr_node = my_queue.pop(0)
+            result.append(curr_node.data)
+            if curr_node.right:
+                my_queue.append(curr_node.right)
+            if curr_node.left:
+                my_queue.append(curr_node.left)
+        result.reverse()
+        return result
+
+    """
+    class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        result = []
+        if not root:
+            return result
+        my_queue = [root]
+        
+        while my_queue:
+            count = len(my_queue)
+            temp = []
+            while count>0:
+                curr_node = my_queue.pop(0)
+                temp.append(curr_node.val)
+                count -= 1
+                if curr_node.left:
+                    my_queue.append(curr_node.left)
+                if curr_node.right:
+                    my_queue.append(curr_node.right)
+            result.append(temp)   
+        result.reverse()
+        return result
+    """
+
+
 
     def count(self):
         if not self.root:
@@ -145,9 +275,23 @@ def test_tree():
     #print(tree_2.count())
     #print(tree_2.height(tree_2.root))
     #print(tree_2.count_using_stack())
-    print(tree_2.pre_order_stack_print())
-    print(tree_2.in_order_stack_print())
+    #print(tree_2.pre_order_stack_print())
+    #print(tree_2.in_order_stack_print())
+    #print(tree_2.post_order_stack_print())
+    #print(tree_2.post_order_stack_print_2())
+    root_3 = Node(1)
+    tree_3 = Binary_Tree(root_3)
+    root_3.left = Node(2)
+    root_3.right = Node(3)
+    root_3.left.left = Node(4)
+    root_3.left.right = Node(5)
+    root_3.right.left = Node(6)
+    root_3.right.right = Node(7)
+    print(tree_3.post_order_stack_print_2())
+    print(tree_2.post_order_stack_print_3())
+    print(tree_2.level_print())
+    print(tree_2.in_order_print(root))
+    print(tree_2.level_print_2())
 
 test_tree()
 
-#calculate how many nodes a tree has
